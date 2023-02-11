1d3754b0ad8b5b34d4424f94ff983995ba20d29c
Enrique <enrique.barbosasilva@gmail.com>
2023-02-11 00:07:52 -0300

    feat: for-style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,29 @@
+import { HeaderStyled } from "./style";
+import { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/User/UserContext";
+
 const Header = () => {
-  return <></>;
+  const { isLogged, setIsLogged } = useContext(UserContext);
+
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.clear();
+
+    setIsLogged(false);
+    navigate("/", { replace: true });
+  };
+
+  return (
+    <>
+      <HeaderStyled>
+        {" "}
+        <h1>Inventory Control</h1>{" "}
+        {isLogged ? <button onClick={logout}>Sair</button> : null}
+      </HeaderStyled>
+    </>
+  );
 };
+
+export default Header;
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,1 +1,62 @@
 import styled from "styled-components";
+
+export const HeaderStyled = styled.header`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  padding: 10px;
+  background-color: #1c2d3e;
+  align-items: center;
+  h1 {
+    color: #ffffff;
+    font-size: 13px;
+    font-weight: bold;
+  }
+  button {
+    height: 2em;
+    width: 50px;
+    background-color: #016dd9;
+    color: #ffffff;
+    border-radius: 5px;
+    -webkit-animation: jello-horizontal 0.9s both;
+    animation: jello-horizontal 0.9s both;
+    border: 2px solid #016dd9;
+    outline: none;
+    font-size: 17px;
+  }
+  button:hover {
+    background: #016dd9;
+    color: #ffffff;
+    animation: squeeze3124 0.9s both;
+  }
+  @keyframes squeeze3124 {
+    0% {
+      -webkit-transform: scale3d(1, 1, 1);
+      transform: scale3d(1, 1, 1);
+    }
+    30% {
+      -webkit-transform: scale3d(1.25, 0.75, 1);
+      transform: scale3d(1.25, 0.75, 1);
+    }
+    40% {
+      -webkit-transform: scale3d(0.75, 1.25, 1);
+      transform: scale3d(0.75, 1.25, 1);
+    }
+    50% {
+      -webkit-transform: scale3d(1.15, 0.85, 1);
+      transform: scale3d(1.15, 0.85, 1);
+    }
+    65% {
+      -webkit-transform: scale3d(0.95, 1.05, 1);
+      transform: scale3d(0.95, 1.05, 1);
+    }
+    75% {
+      -webkit-transform: scale3d(1.05, 0.95, 1);
+      transform: scale3d(1.05, 0.95, 1);
+    }
+    100% {
+      -webkit-transform: scale3d(1, 1, 1);
+      transform: scale3d(1, 1, 1);
+    }
+  }
+`;
