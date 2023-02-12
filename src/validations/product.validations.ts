03bb9b7e99508fb7d056d26360aae45d405a50f2
Enrique <enrique.barbosasilva@gmail.com>
2023-02-11 23:31:44 -0300

    feat: new-validation

--- src/validations/product.validations.ts
+++ src/validations/product.validations.ts
@@ -6,3 +6,10 @@ export const productSchema = yup.object().shape({
   description: yup.string().required("Descrição obrigatória"),
   price: yup.number().required("Preço é obrigatório"),
 });
+
+export const productPatchSchema = yup.object().shape({
+  name: yup.string(),
+  category: yup.string(),
+  description: yup.string(),
+  price: yup.number(),
+});
